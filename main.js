4eab8d8a3d1a424d3eabaf075b93d9fd1682e14a
younghong <durtiful82@gmail.com>
2021-02-23 18:42:56 +0900

    ani

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-var canvas;
+var design;
 
 start = () => {
 
     // create a wrapper around native canvas element (with id="c")
-    this.canvas = new fabric.Canvas('c', {
-        backgroundColor: 'rgb(100,100,200)',
+    this.design = new fabric.Canvas('c', {
+        backgroundColor: 'rgb(218,100,200)',
         selectionColor: 'blue',
         selectionLineWidth: 2
         // ...
@@ -13,13 +13,13 @@ start = () => {
 
     addScrollEvent();
 
-    this.canvas.add(
+    this.design.add(
         new fabric.Circle({ top: 140, left: 230, radius: 25, fill: 'green' }),
     );
 
     var path = new fabric.Path('M 0 0 L 20 0 L 20 20 z');
     path.set({ left: 120, top: 120 });
-    this.canvas.add(path);
+    this.design.add(path);
 
     var textWithStroke = new fabric.Text("Text with a stroke", {
         stroke: '#ff1318',
@@ -31,25 +31,28 @@ start = () => {
         strokeWidth: 3
       });
 
-      this.canvas.add(textWithStroke);
-      this.canvas.add(loremIpsumDolor);
+      this.design.add(textWithStroke);
+      this.design.add(loremIpsumDolor);
 }
 
 addScrollEvent = () => {
 
-    this.canvas.on('mouse:wheel', function(opt) {
+    this.design.on('mouse:wheel', function(opt) {
         var delta = opt.e.deltaY;
-        var zoom = canvas.getZoom();
+        var zoom = design.getZoom();
         zoom *= 0.999 ** delta;
         if (zoom > 20) zoom = 20;
         if (zoom < 0.01) zoom = 0.01;
-        canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+        design.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
         opt.e.preventDefault();
         opt.e.stopPropagation();
+
+        updateMiniMap();
+
       });
 
 
-      this.canvas.on('mouse:down', function(options) {
+      this.design.on('mouse:down', function(options) {
         if (options.target) {
           console.log('an object was clicked! ', options.target.type);
           animation(options.target);
@@ -77,7 +80,7 @@ animation = (target) => {
         duration: 1000,
         onChange: function(value) {
             target.left = value;
-            canvas.renderAll();
+            design.renderAll();
         },
 
       });
@@ -86,11 +89,11 @@ animation = (target) => {
 
 function animate() {
 
-    var max=400-canvas.item(0).height;
+    var max=400-design.item(0).height;
 
-    canvas.item(0).animate('top', canvas.item(0).get('top') === max ? '0' : max, {
+    design.item(0).animate('top', design.item(0).get('top') === max ? '0' : max, {
       duration: 1000,
-      onChange: canvas.renderAll.bind(canvas),
+      onChange: design.renderAll.bind(design),
       onComplete: animate
     });
   }
@@ -108,10 +111,94 @@ addRect = (x,y) => {
     });
 
     // "add" rectangle onto canvas
-    this.canvas.add(rect);
+    this.design.add(rect);
 }
 
 
 
 start();
 animate();
+
+
+var minimap = new fabric.Canvas('minimap',  { containerClass: 'minimap', selection: false });
+
+
+function createCanvasEl() {
+  var designSize = { width: 400, height: 400 };
+  var originalVPT = design.viewportTransform;
+  // zoom to fit the design in the display canvas
+  var designRatio = fabric.util.findScaleToFit(designSize, design);
+
+  // zoom to fit the display the design in the minimap.
+  var minimapRatio = fabric.util.findScaleToFit(design, minimap);
+
+  var scaling = minimap.getRetinaScaling();
+
+  var finalWidth =  designSize.width * designRatio;
+  var finalHeight =  designSize.height * designRatio;
+
+  design.viewportTransform = [
+    designRatio, 0, 0, designRatio,
+    (design.getWidth() - finalWidth) / 2,
+    (design.getHeight() - finalHeight) / 2
+  ];
+  var canvas = design.toCanvasElement(minimapRatio * scaling);
+  design.viewportTransform = originalVPT;
+  return canvas;
+}
+
+function updateMiniMap() {
+  var canvas = createCanvasEl();
+  minimap.backgroundImage._element = canvas;
+  minimap.requestRenderAll();
+}
+
+function updateMiniMapVP() {
+  var designSize = { width: 400, height: 400 };
+  var rect = minimap.getObjects()[0];
+  var designRatio = fabric.util.findScaleToFit(designSize, design);
+  var totalRatio = fabric.util.findScaleToFit(designSize, minimap);
+  var finalRatio = designRatio / design.getZoom();
+  rect.scaleX = finalRatio;
+  rect.scaleY = finalRatio;
+  rect.top = minimap.backgroundImage.top - design.viewportTransform[5] * totalRatio / design.getZoom();
+  rect.left = minimap.backgroundImage.left - design.viewportTransform[4] * totalRatio / design.getZoom();
+  minimap.requestRenderAll();
+}
+
+
+function initMinimap() {
+  var canvas = createCanvasEl();
+  var backgroundImage = new fabric.Image(canvas);
+  backgroundImage.scaleX = 1 / design.getRetinaScaling();
+  backgroundImage.scaleY = 1 / design.getRetinaScaling();
+  minimap.centerObject(backgroundImage);
+  minimap.backgroundColor = 'white';
+  minimap.backgroundImage = backgroundImage;
+  minimap.requestRenderAll();
+  var minimapView = new fabric.Rect({
+    top: backgroundImage.top,
+    left: backgroundImage.left,
+    width: backgroundImage.width / design.getRetinaScaling(),
+    height: backgroundImage.height/ design.getRetinaScaling(),
+    fill: 'rgba(0, 0, 255, 0.3)',
+    cornerSize: 6,
+    transparentCorners: false,
+    cornerColor: 'blue',
+    strokeWidth: 0,
+  });
+  minimapView.controls = {
+    br: fabric.Object.prototype.controls.br,
+  };
+  minimap.add(minimapView);
+}
+
+//var debouncedMiniMap = _.debounce(updateMiniMap, 250);
+
+design.on('object:modified', function() {
+  updateMiniMap();
+})
+
+initMinimap();
+updateMiniMapVP();
+
---
+++ main4.js
@@ -0,0 +1,42 @@
+(function() {
+    var canvas = this.__canvas = new fabric.Canvas('c');
+
+
+    window.addEventListener("keydown", (e) => {
+        canvas.getActiveObject();
+        console.log(e)
+    } )
+
+    fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
+    fabric.Object.prototype.transparentCorners = false;
+  
+    // for (var i = 0, len = 5; i < len; i++) {
+    //   for (var j = 0, jlen = 5; j < jlen; j++) {
+    //     fabric.Sprite.fromURL('/img/sprite.png', createSprite(i, j));
+    //   }
+    // }
+    fabric.Sprite.fromURL('/img/sprite.png', createSprite(0, 0));
+  
+    function createSprite(i, j) {
+      return function(sprite) {
+        sprite.set({
+          left: i * 100 + 50,
+          top: j * 100 + 50,
+          angle: fabric.util.getRandomInt(-30, 30)
+        });
+        canvas.add(sprite);
+        setTimeout(function() {
+          sprite.set('dirty', true);
+          sprite.play();
+        }, fabric.util.getRandomInt(1, 10) * 100);
+      };
+    }
+  
+    (function render() {
+      canvas.renderAll();
+      fabric.util.requestAnimFrame(render);
+    })();
+
+
+
+  })();
